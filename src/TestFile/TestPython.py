bb1ccda8799df6e9177a7af25087aa0cc9f81a26
Srivathsa Mantrala <srivathsamv2@gmail.com>
2025-10-09 12:39:53 -0400

    Tested Python integration and cleaned data set for test

---
+++ src/TestFile/TestPython.py
@@ -0,0 +1,7 @@
+import pandas as pd
+import numpy as np
+import seaborn as sns
+import matplotlib.pyplot as plt
+from scipy import stats
+
+print("✅ Python environment working perfectly inside IntelliJ!")
